767a652edb0a5d395a7ea54dfd151d165e25d3cf
svs-praveen <svs.praveen@gmail.com>
2015-07-17 13:03:39 +0100

    Pushing my changes to my Git Hub Repository

---
+++ src/main/java/personal/message/App.java
@@ -0,0 +1,18 @@
+package personal.message;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static String getWelcomeMessage() {
+
+        return "Welcome to Git Hub";
+    }
+}
